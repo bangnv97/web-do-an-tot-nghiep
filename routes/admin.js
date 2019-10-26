5f377511136f23109dffd9c0d19de98ac7d5d551
bangnv97 <darkraipy@gmail.com>
2019-10-26 20:16:56 +0700

    fix api create and add detail product

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,7 @@
-const Product = require('../models/product');
 const express = require('express');
+const multer = require('multer');
 const adminController = require('../controllers/admin');
 const router = express.Router();
-
-// /admin/products => GET
-router.get('/products', adminController.getProducts);
-
-router.post('/product', adminController.createProduct);
 
 router.get('/getProducts', adminController.getPagProducts)
 
@@ -16,7 +11,9 @@ router.put('/product/:id', adminController.putProduct);
 
 router.delete('/product/:id', adminController.deleteProduct);
 
-module.exports = router;
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
+module.exports = router;
 
 
